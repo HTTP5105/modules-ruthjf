2891cd78acf8a7952fd190a96adf229218a45d84
ruthjf <74483177+ruthjf@users.noreply.github.com>
2021-06-01 23:48:43 -0400

    Add files via upload
    adding another-module.js and my-module.js

---
+++ public/my-module.js
@@ -0,0 +1,6 @@
+// import theShire and buckland
+// export theSHire and statement
+
+var theShire = new HobbitSettlement('The Shire', 1601, ['Marcho', 'Blanco']);
+
+var statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
