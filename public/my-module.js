9158a35c5301c18aa0576f97a2cc4cab923bec3a
ruthjf <74483177+ruthjf@users.noreply.github.com>
2021-06-02 00:43:19 -0400

    Add files via upload

--- public/my-module.js
+++ public/my-module.js
@@ -1,8 +1,4 @@
-// import theShire and buckland
-// export theSHire and statement
-
 import { theShire, buckland } from './another-module.js';
 
+const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
 export { theShire, statement };
-
-var statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
