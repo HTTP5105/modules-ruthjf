09ad8e5520bc520ea755eccbea3fbc1f525bc147
ruthjf <74483177+ruthjf@users.noreply.github.com>
2021-06-02 00:28:26 -0400

    Add files via upload

--- public/my-module.js
+++ public/my-module.js
@@ -1,7 +1,6 @@
 // import theShire and buckland
 // export theSHire and statement
 
-var theShire = new HobbitSettlement('The Shire', 1601, ['Marcho', 'Blanco']);
 import { theShire, buckland } from './another-module.js';
 
 export { theShire, statement };
